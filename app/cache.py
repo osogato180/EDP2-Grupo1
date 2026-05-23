a98053e0e4ee98952e29b41d09a59e6b3a287356
osogato180 <osogato180@gmail.com>
2026-05-22 22:23:04 -0500

    Update cache.py

--- app/cache.py
+++ app/cache.py
@@ -1,1 +1,10 @@
+import redis
+import os
 
+r = redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
+
+def get_cached_response(key):
+    return r.get(key)
+
+def set_cached_response(key, value):
+    r.set(key, value, ex=3600)
